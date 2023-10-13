05db3ead71ba7ba716d4194740cd098d9f4dc91c
Alexandre Bonte <alexandre@mbpdealexandre.home>
2023-10-13 08:35:59 +0200

    new message

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello surfy app !!!');
+  res.end('Hello ! Ready for surfing ?');
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Welcome Terraform Surfy app`);
+    console.log(`Welcome Node Surfy app`);
     console.log(`Server running at http://${hostname}:${port}/`);
 });
