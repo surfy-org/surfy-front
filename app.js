abd5934c7b1a1ea81465a24df484e3eb5794db9b
Alexandre Bonte <alexandre@MacBook-Pro-de-Alexandre.local>
2023-10-16 15:48:49 +0200

    hello

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hello! `);
+  res.end('Hello alex !');
 });
 
 server.listen(port, ip, () => {
