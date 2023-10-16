ae0770e5f81f03839921499f2b988c5b89b9b154
Alexandre Bonte <alexandre@MacBook-Pro-de-Alexandre.local>
2023-10-16 13:55:07 +0200

    simple message

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const http = require('http');
 
 const ip = '0.0.0.0';
 const port = 3000;
-const hostname = os.hostname();
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hello! Your host is : ${hostname}`);
+  res.end(`Hello! `);
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, ip, () => {
     console.log(`Welcome Node Surfy app`);
     console.log(`Server running at http://${ip}:${port}/`);
 });
