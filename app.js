7bb34ebaf27917735681c4fa079030a716ed6da5
Alexandre Bonte <alexandre@mbpdealexandre.home>
2023-10-17 16:42:09 +0200

    add wello

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const hostname = os.hostname();
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hello Fanny-Funny ! Your host is : ${hostname}`);
+  res.end(`Hello Wello ! Your host is : ${hostname}`);
 });
 
 server.listen(port, ip, () => {
