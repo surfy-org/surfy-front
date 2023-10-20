f7b543e83d0bec34a48575a48c0668ae62fd12cd
Alexandre Bonte <alexandre@MacBook-Pro-de-Alexandre.local>
2023-10-20 11:26:39 +0200

    Hey man deploy

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const hostname = os.hostname();
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hey Alex ! Your host is : ${hostname}`);
+  res.end(`Hey man ! Your host is : ${hostname}`);
 });
 
 server.listen(port, ip, () => {
