f69798a2c4921a6e3282635ae4c97181d38980ef
robbee <alexandre.bonte@gmail.com>
2023-11-08 17:58:32 +0100

    change to wello

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const hostname = os.hostname();
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hey kingos ! Your host is : ${hostname}`);
+  res.end(`Hey wello ! Your host is : ${hostname}`);
 });
 
 server.listen(port, ip, () => {
